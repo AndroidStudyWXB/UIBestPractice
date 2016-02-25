b5f2c52c381a36a63654d06e9e7bb2a4251c04e6
WXB506 <1922686887@qq.com>
2016-02-25 08:12:28 +0800

    initail commit

---
+++ app/src/main/java/com/study/uibestpractice/Msg.java
@@ -0,0 +1,26 @@
+package com.study.uibestpractice;
+
+/**
+ * Created by WXB506 on 2016/2/25.
+ */
+public class Msg {
+
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SEND     = 1;
+
+    private String content;
+    private int type;
+
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
